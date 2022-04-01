3d2ff63b2382813022b0b0286a2a39f5e066bd12
weygerus <gabrileao38@gmail.com>
2022-04-01 04:06:10 -0300

    01/04/2022-1

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
-const express = require("express");    // chamada do express(Server)
-const app = express();                 // instanciação do servidor
-const data = require("./data.json")    // chamada dos dados
+const express = require("express");       // chamada do express(Server)
+const app     = express();                // instanciação do servidor
+const data    = require("./data.json")    // chamada dos dados
 
 
 app.listen(3000, function(){           // Início do servidor
     console.log("servidor ativo")  
 });
-
 app.use(express.json());               // indicação de que o express usará a notação json
 
-// requisições
-app.get("/clientes", function(req, res){
+
+// Requisições
+app.get("/clientes", function(req, res) {
     res.json(data);
 });
 
-app.get("/clientes/:id", function(req, res){});
+app.get("/clientes/:id", function(req, res) {
+    const { id } = req.params;
+    const cliente = data.find(cli => cli.id == id);
+    
+    if(cliente){
+        res.json(cliente);
+    }
+    else{
+        return res.status(204).json();
+    }
+});
 
 /* 
  :id - parametro para receber cliente especifico
@@ -45,5 +55,5 @@ Verbos HTTP:
   http://localhost:3000/clientes
 
   localhost:3000/clientes - URI (forma de alcançar o resource)
-  clients - endpoint (nome do resource)
+  clientes - endpoint (nome do resource)
 */
